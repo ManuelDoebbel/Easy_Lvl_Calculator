db966e95e1c5c2917cedea02ec3fc92b7955631f
N00bY24 <138362349+N00bY24@users.noreply.github.com>
2024-04-14 17:23:54 +0200

    The easy LevelUp_Calculator

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿namespace LevelUp_Calculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DisplayCommand();
+            LevelUpInput();
+            DisplayCurrentLevel();
+        }
+
+        static int InputUserXP = -1;
+        static string input;
+
+        private static void DisplayCommand()
+        {
+            Console.Write("Input XP: ");
+        }
+
+        private static void LevelUpInput()
+        {
+            input = Console.ReadLine();
+            bool success = int.TryParse(input, out InputUserXP);
+            if (success)
+            {
+                LvlUps(InputUserXP);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+
+        enum LvlUp
+        {
+            None = 0,
+            Level1 = 20,
+            Level2 = 50,
+            Level3 = 100,
+            Level4 = 250,
+            Level5 = 600,
+            Level6 = 1400,
+            Level7 = 3000,
+            Level8 = 6000,
+            Level9 = 11000,
+        }
+
+        private static LvlUp LvlUps(int InputUserXP)
+        {
+            if (InputUserXP >= 11000)
+            {
+                return LvlUp.Level9;
+            }
+            else if (InputUserXP >= 6000)
+            {
+                return LvlUp.Level8;
+            }
+            else if (InputUserXP >= 3000)
+            {
+                return LvlUp.Level7;
+            }
+            else if (InputUserXP >= 1400)
+            {
+                return LvlUp.Level6;
+            }
+            else if (InputUserXP >= 600)
+            {
+                return LvlUp.Level5;
+            }
+            else if (InputUserXP >= 250)
+            {
+                return LvlUp.Level4;
+            }
+            else if (InputUserXP >= 100)
+            {
+                return LvlUp.Level3;
+            }
+            else if (InputUserXP >= 50)
+            {
+                return LvlUp.Level2;
+            }
+            else if (InputUserXP >= 20)
+            {
+                return LvlUp.Level1;
+            }
+            else
+            {
+                return LvlUp.None;
+            }
+        }
+
+        private static void DisplayCurrentLevel()
+        {
+            Console.WriteLine("Current level: " + LvlUps(InputUserXP));
+        }
+    }
+}
